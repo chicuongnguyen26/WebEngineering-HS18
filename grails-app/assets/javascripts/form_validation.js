080c8cb45eb43a74882337ddb5715dcdbae042d0
Christian Ribeaud <christian.ribeaud@fhnw.ch>
2018-10-12 18:35:07 +0200

    Register 'onchange' listeners in 'calc.gsp'

--- grails-app/assets/javascripts/form_validation.js
+++ grails-app/assets/javascripts/form_validation.js
@@ -1,18 +1,19 @@
 // Validating an input field that accepts numerical values between 1.0 and 6.0
 
-function validate(inputField) {
-  if (handledError(inputField.value < 1.0, inputField, ' value needs to be at least 1.0'))
-      return;
-  if (handledError(inputField.value > 6.0, inputField, ' value needs to be at most 6.0'))
-      return;
-  inputField.setAttribute('class','');
+function validate(event) {
+    var inputField = event.target;
+    if (handledError(inputField.value < 1.0, inputField, ' value needs to be at least 1.0'))
+        return;
+    if (handledError(inputField.value > 6.0, inputField, ' value needs to be at most 6.0'))
+        return;
+    inputField.setAttribute('class', '');
 }
 
 function handledError(check, inputField, msg) {
-  if (check) {
-      inputField.setAttribute('class','error');
-      alert(inputField.name + msg);
-      inputField.focus();
-  }
-  return check;
+    if (check) {
+        inputField.setAttribute('class', 'error');
+        alert(inputField.name + msg);
+        inputField.focus();
+    }
+    return check;
 }
