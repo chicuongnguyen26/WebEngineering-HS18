0b615977871a9d28fcf1750a9448d9efe5e54005
Christian Ribeaud <christian.ribeaud@fhnw.ch>
2018-10-19 11:41:32 +0200

    Add initial state before playing around

---
+++ src/test/groovy/mvc/MultiplicationCircleControllerSpec.groovy
@@ -0,0 +1,23 @@
+package mvc
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+/**
+ * See the API for {@link ControllerUnitTest} for usage instructions.
+ */
+class MultiplicationCircleControllerSpec extends Specification implements ControllerUnitTest<MultiplicationCircleController> {
+
+    void "asking for #segments segments should provide #segments lines"(segments) {
+        given:
+            def model = new MultiplicationCircleModel(segmentCount: segments) // TODO: add the table base when it is available
+        when:
+            controller.index(model)
+        then:
+            model.lines.size() == segments
+        where:
+            segments << [0, 1, 10, 100]
+    }
+
+
+}
