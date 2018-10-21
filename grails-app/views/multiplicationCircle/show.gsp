d8e7819855bafd9ba301ddba3ee8c3724e0d69b4
Christian Ribeaud <christian.ribeaud@fhnw.ch>
2018-10-21 12:27:15 +0200

    Add test to 'MultiplicationCircleSpec'

--- grails-app/views/multiplicationCircle/show.gsp
+++ grails-app/views/multiplicationCircle/show.gsp
@@ -22,7 +22,7 @@
         }
         function decrease(valueName) {
             var input = document.getElementById(valueName);
-            input.value = parseInt(input.value) + 1;
+            input.value = parseInt(input.value) - 1;
         }
     </script>
 </head>
--- src/integration-test/groovy/mvc/MultiplicationCircleSpec.groovy
+++ src/integration-test/groovy/mvc/MultiplicationCircleSpec.groovy
@@ -19,17 +19,32 @@ class MultiplicationCircleSpec extends GebSpec {
 
         when: "click to bump value up"
         $("#segmentCountUp").click()
-
         then: "the field is bumped up"
         $("form").segmentCount == "11"
 
         when: "click to bump value down"
         $("#segmentCountDown").click()
-
-        then: "the field is bumped up"
+        then: "the field is bumped down"
         $("form").segmentCount == "10"
     }
 
-//  add a test that checks the form for the table base
+    void "Changing table base count with JavaScript"() {
+        when:
+        browser.driver.javascriptEnabled = true
+        go '/multiplicationCircle.html'
+        then:
+        title == "Multiplication Circle"
+        $("form").tableBase == "2"
+
+        when: "click to bump value up"
+        $("#tableBaseUp").click()
+        then: "the field is bumped up"
+        $("form").tableBase == "3"
+
+        when: "click to bump value down"
+        $("#tableBaseDown").click()
+        then: "the field is bumped down"
+        $("form").tableBase == "2"
+    }
 
 }
