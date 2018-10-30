cff4e0660659a5e38c7a745807b288d67eb75d47
Christian Ribeaud <christian.ribeaud@fhnw.ch>
2018-10-30 08:08:24 +0100

    Connect the views

--- grails-app/controllers/mvc/BookingController.groovy
+++ grails-app/controllers/mvc/BookingController.groovy
@@ -5,31 +5,33 @@ class BookingController {
     static scaffold = Booking
 
     def forDierk() {
-	    Person dierk = Person.findByEmail("dierk.koenig@fhnw.ch")
-	    List<Booking> result = Booking.findAllByBooker(dierk);
-	    render text: result.toString()
+        Person dierk = Person.findByEmail("dierk.koenig@fhnw.ch")
+        List<Booking> result = Booking.findAllByBooker(dierk);
+        // render text: result.toString()
+        render view: "OpenBookingsFor", model: [bookings: result]
     }
 
-	def all() {
-		respond Booking.list()
-	}
+    def all() {
+        respond Booking.list()
+    }
 
     def freeTodayMorning() {
-	
-		// allRooms = find all rooms	
-		List<Room> allRooms = Room.findAll()
-		
-		// allBookings for this morning
-		Date today = new Date().clearTime()
-		String morning = Booking.AM
-		List<Booking> allBookingsThisMorning = Booking.findAllByDateAndSlot(today, morning);
-		
-		// allRoomsBookedThisMorning
-		List<Room> allRoomsBookedThisMorning = allBookingsThisMorning*.room
-		
-		List<Room> result = allRooms - allRoomsBookedThisMorning
-	
-	    respond result
+
+        // allRooms = find all rooms
+        List<Room> allRooms = Room.findAll()
+
+        // allBookings for this morning
+        Date today = new Date().clearTime()
+        String morning = Booking.AM
+        List<Booking> allBookingsThisMorning = Booking.findAllByDateAndSlot(today, morning);
+
+        // allRoomsBookedThisMorning
+        List<Room> allRoomsBookedThisMorning = allBookingsThisMorning*.room
+
+        List<Room> result = allRooms - allRoomsBookedThisMorning
+
+        // respond result
+        render view: "AvailableRoomsFor", model: [rooms: result]
     }
 
 }
