17cb0d89bc796915a45d8c5064d4a713adf4af25
Christian Ribeaud <christian.ribeaud@fhnw.ch>
2018-11-06 10:00:10 +0100

    Add domain classes

---
+++ grails-app/domain/mvc/Room.groovy
@@ -0,0 +1,17 @@
+package mvc
+
+// @Resource
+class Room {
+
+    String name
+    int max
+
+    String toString() {
+        "$name ($max)"   // Groovy :-)
+    }
+
+    static constraints = {
+        name(blank: false)
+        max(min: 1)
+    }
+}
