585e77b15e55218ca90ef31f43c9f7f109d5367c
Christian Ribeaud <christian.ribeaud@fhnw.ch>
2018-11-11 12:11:54 +0100

    Add logout functionality

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,9 +1,14 @@
-// For '/dbconsole'
-grails.plugin.springsecurity.rejectIfNoRule = false
-grails.plugin.springsecurity.fii.rejectPublicInvocations = false
+// For any URL NOT defined as static rule
+// grails.plugin.springsecurity.rejectIfNoRule = false
+// grails.plugin.springsecurity.fii.rejectPublicInvocations = false
+
 // For basic authentication
-grails.plugin.springsecurity.useBasicAuth = true
-grails.plugin.springsecurity.basic.realmName = "HTTP Basic Auth Demo"
+// grails.plugin.springsecurity.useBasicAuth = true
+// grails.plugin.springsecurity.basic.realmName = "HTTP Basic Auth Demo"
+
+// For logout functionality
+grails.plugin.springsecurity.logout.postOnly = false
+grails.plugin.springsecurity.logout.afterLogoutUrl = "/"
 
 // Added by the Spring Security Core plugin:
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'mvc.User'
@@ -11,6 +16,7 @@ grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'mvc.UserRole'
 grails.plugin.springsecurity.authority.className = 'mvc.Role'
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/',               access: ['permitAll']],
+	[pattern: '/dbconsole/**',   access: ['permitAll']],
 	[pattern: '/error',          access: ['permitAll']],
 	[pattern: '/index',          access: ['permitAll']],
 	[pattern: '/index.gsp',      access: ['permitAll']],
@@ -24,6 +30,7 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 
 grails.plugin.springsecurity.filterChain.chainMap = [
 	[pattern: '/assets/**',      filters: 'none'],
+	[pattern: '/dbconsole/**',   filters: 'none'],
 	[pattern: '/**/js/**',       filters: 'none'],
 	[pattern: '/**/css/**',      filters: 'none'],
 	[pattern: '/**/images/**',   filters: 'none'],
